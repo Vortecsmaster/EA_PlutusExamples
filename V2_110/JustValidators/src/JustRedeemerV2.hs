27531d8c6049c37485c97a48af84e0a41a69e6a4
Vortecsmaster <vortecsmaster@vortecsla.com>
2023-01-28 09:23:22 -0500

    Including v1.1.0 separated folder

--- V2_110/JustValidators/src/JustRedeemerV2.hs
+++ V2_110/JustValidators/src/JustRedeemerV2.hs
@@ -8,7 +8,7 @@
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 
-module CustomTypedValidatorV2 where
+module JustRedeemerV2 where
 
 --PlutusTx 
 import           PlutusTx                       (Data (..))
@@ -21,9 +21,8 @@ import           Plutus.Contract
 import           Ledger                         hiding (singleton)
 import qualified Ledger.Address                 as V1Address
 import           Ledger.Constraints             as Constraints              -- Same library name, different functions for V1 and V2 in some cases
---import qualified Ledger.Typed.Scripts         as Scripts              
-import qualified Plutus.Script.Utils.V2.Typed.Scripts as Scripts            -- New library name for Typed Validators and some new fuctions
-import qualified Plutus.V2.Ledger.Api                 as PlutusV2            
+--import qualified Ledger.Scripts               as Scripts               
+import qualified Plutus.Script.Utils.V2.Scripts as Scripts                  -- New library name for Typed Validators and some new fuctions
 import           Ledger.Ada                     as Ada
 --Trace Emulator
 import           Plutus.Trace
@@ -42,87 +41,83 @@ import           Prelude                        (IO, Semigroup (..), String, sho
 import           Text.Printf                    (printf)
 import           Control.Monad.Freer.Extras     as Extras
 
-{-
-import Language.Haskell.TH (RuleBndr(TypedRuleVar))
--}
-
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 --THE ON-CHAIN CODE
 
--- newtype MyWonderfullRedeemer = MWR Integer
+{-# INLINABLE justRedeemer #-} 
+justRedeemer :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+justRedeemer _ redeemer _ 
+             | redeemer == Builtins.mkI 42 = ()
+             | otherwise                   = traceError "Wrong Redeemer"
 
--- PlutusTx.makeIsDataIndexed ''MyWonderfullRedeemer [('MWR,0)]        -- At compile time write an instance of this data type (MyWonderFullRedeemer) on the IsData typeclass
 
-data MyWonderfullRedeemer = MWR Integer
-PlutusTx.unstableMakeIsData ''MyWonderfullRedeemer
+{-# INLINABLE justValidator #-}
+justValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+justValidator datum redeemer _ 
+    | redeemer == datum             = ()
+    | redeemer Builtins.mkI 999     = ()
+    | otherwise                   = traceError "Wrong Redeemer"
 
 
-{-# INLINABLE typedRedeemer #-} 
-typedRedeemer :: () -> MyWonderfullRedeemer -> PlutusV2.ScriptContext -> Bool   
-typedRedeemer () (MWR redeemer) _ = traceIfFalse "Wrong redeemer!" (redeemer == 42)
- 
+{-# INLINABLE justLoan #-}
+justLoan :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+justLoan datum action _ 
+    | action == REQUEST     = {find the borrower NFT && verify enough collateral && validate enough to cover collatera and fees}
+    | action == LEND        = {validate lender NFT && lender provided enough value to cover loan && verify enough collateral available && validate enough to cover collatera and fees}
+    | action == REPAYMENT   = ...
+    | action == MARGINCALL  = ...
+    | otherwise                   = traceError "Unknown action"
 
-data Typed                                                          -- New type that encode the information about the Datum and the Redeemer
-instance Scripts.ValidatorTypes Typed where
-     type instance RedeemerType Typed = MyWonderfullRedeemer
-     type instance DatumType Typed = ()
-
-typedValidator :: Scripts.TypedValidator Typed
-typedValidator = Scripts.mkTypedValidator @Typed      
-    $$(PlutusTx.compile [|| typedRedeemer ||]) 
-    $$(PlutusTx.compile [|| wrap ||])                               
-  where
-    wrap = Scripts.mkUntypedValidator  @() @MyWonderfullRedeemer     --New wrapper function for typed validators             
-    
 
 validator :: Validator
-validator = Scripts.validatorScript typedValidator                   -- Get the untyped validator script of the wrapped typeValidator PlutusCore
+validator = mkValidatorScript $$(PlutusTx.compile [|| justRedeemer ||])
 
 valHash :: Ledger.ValidatorHash
-valHash = Scripts.validatorHash typedValidator                      
+valHash = Scripts.validatorHash validator  -- just the hash of the validator
 
-scrAddress :: Ledger.Address
-scrAddress = Scripts.validatorAddress typedValidator                 -- New functino to derive the address, included in the Utils library
+scrAddress :: V1Address.Address
+scrAddress = V1Address.scriptHashAddress valHash          -- Couldn't find a new version of scriptAddress for unTyped Scripts
+--replaces:
 --scrAddress = scriptAddress validator 
 
 
 --THE OFFCHAIN CODE
 
 type GiftSchema =
-            Endpoint "give" Integer  --
-        .\/ Endpoint "grab" Integer
+              Endpoint "give" Integer  
+          .\/ Endpoint "grab" Integer
 
 give :: AsContractError e => Integer -> Contract w s e ()
-give amount = do                                                                            -- mustPayToTheScriptWithDatumInTx
-    let tx = mustPayToTheScriptWithDatumHash () $ Ada.lovelaceValueOf amount               -- Correction pending on ...Typed version for one script, This Tx needs an output, thats its going to be the Script Address, Datum MUST be specified, so unit ().
-    ledgerTx <- submitTxConstraints typedValidator tx                                                                          --This line submit the Tx
+give amount = do
+    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount      --This Tx needs an output, thats its going to be the Script Address, Datum MUST be specified, so is created and the ammount of lovelaces
+    ledgerTx <- submitTx tx                                                                          --This line submit the Tx
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx                                                --This line waits for confirmation
     Plutus.Contract.logInfo @String $ printf "made a gift of %d lovelace" amount                                     --This line log info,usable on the PP(Plutus Playground)
     
---grab :: forall w s e. AsContractError e => Contract w s e ()                                     
-grab :: forall w s e. AsContractError e => Integer -> Contract w s e ()                                     
+                                   
+grab :: forall w s e. AsContractError e => Integer -> Contract w s e ()                              -- The Contract  s=Schema  e=Error or value restricted from AsContractError                                 
 grab n = do
     utxos <- utxosAt scrAddress                                                                      -- This will find all UTXOs that sit at the script address
     let orefs   = fst <$> Map.toList utxos                                                           -- This get all the references of the UTXOs
         lookups = Constraints.unspentOutputs utxos      <>                                           -- Tell where to find all the UTXOS
-                  Constraints.plutusV2OtherScript validator                                            -- and inform about the actual validator (the spending tx needs to provide the actual validator)
+                  Constraints.plutusV2OtherScript validator                                                  -- and inform about the actual validator (the spending tx needs to provide the actual validator)
         tx :: TxConstraints Void Void                                                            
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toBuiltinData (MWR n) | oref <- orefs]  -- Define the TX giving constrains, one for each UTXO sitting on this addrs,
-                                                                                                     -- must provide a redeemer (ignored in this case)
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI n | oref <- orefs]  -- Define the TX giving constrains, one for each UTXO sitting on this addrs,
+                                                                                                     -- must provide a redeemer 
     ledgerTx <- submitTxConstraintsWith @Void lookups tx                                             -- Allow the wallet to construct the tx with the necesary information
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx                                                -- Wait for confirmation
     Plutus.Contract.logInfo @String $ "collected gifts"                                                              -- Log information 
-                                                                                                     -- collectFromTheScript
+
 endpoints :: Contract () GiftSchema Text ()
 endpoints = awaitPromise (give' `select` grab') >> endpoints                                         -- Asynchronously wait for the endpoints interactions from the wallet
   where                                                                                              -- and recursively wait for the endpoints all over again
     give' = endpoint @"give" give                                                                    -- block until give
-    grab' = endpoint @"grab" grab                                                                    -- block until grab
-
+    grab' = endpoint @"grab" grab
+                                                              -- block until grab
 -- Playground broken at the moment - September 2022
 -- mkSchemaDefinitions ''GiftSchema                                                                     -- Generate the Schema for the playground
--- mkKnownCurrencies [] 
+-- mkKnownCurrencies []
 
 --SIMULATION
 
@@ -130,8 +125,8 @@ test :: IO ()
 test = runEmulatorTraceIO $ do
     h1 <- activateContractWallet (Wallet.knownWallet 1) endpoints
     h2 <- activateContractWallet (Wallet.knownWallet 2) endpoints
-    callEndpoint @"give" h1 51000000
+    callEndpoint @"give" h1 $ 51000000
     void $ Emulator.waitNSlots 11
     callEndpoint @"grab" h2 42
     s <- Emulator.waitNSlots 11
-    Extras.logInfo $ "End of Simulation at slot " ++ show s
+    Extras.logInfo $ "End of Simulation at slot " ++ show s                                                                   -- MakeKnown currencies for the playground to have some ADA available
