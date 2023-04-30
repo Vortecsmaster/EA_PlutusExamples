07830be978f86fc4ee1b5da96aecefd2fd6e401b
Roberto <vortecsmaster@vortecsla.com>
2023-04-29 20:47:09 -0500

    Included saveVal function to serialize contract

--- V2_110/Demeter/FromScratch/src/SimpleValidator.hs
+++ V2_110/Demeter/FromScratch/src/SimpleValidator.hs
@@ -1,1 +1,61 @@
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE DataKinds           #-}
+
 module SimpleValidator where
+
+import                  PlutusTx                    (BuiltinData, compile)
+import                  PlutusTx.Builtins           as Builtins (mkI)
+import                  PlutusTx.Prelude            (error, otherwise,(==),Bool)
+import qualified        Plutus.V2.Ledger.Api        as PlutusV2
+import                  Serialise                   (writeValidatorToFile, writeDataToFile)
+
+
+{-# INLINABLE alwaysSucceeds #-}
+alwaysSucceeds :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+alwaysSucceeds _ _ _ = ()
+
+{-# INLINABLE alwaysFails #-}
+alwaysFails :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+alwaysFails _ _ _ = error ()
+
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| alwaysSucceeds ||])
+
+-- {-# INLINABLE withTypes #-}
+-- withTypes ::  Integer -> String -> PlutusV2.ScriptContext -> Bool
+-- withTypes _ _ _ = True
+
+{-
+  fun :: Integer -> String -> PlutusV2.ScriptContext -> Bool  =>  wrapedfun :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+ (String) Text -> BuiltinString  (encodeUtf8 :: BuiltinString -> BuiltinByteString) -> (B ByteString)
+-}
+
+
+
+{-# INLINABLE dvsR #-}
+dvsR ::  BuiltinData -> BuiltinData -> BuiltinData -> ()
+dvsR datum redeemer _ 
+ | datum == redeemer                = ()
+ | otherwise                        = error ()
+
+dvsRvalidator :: PlutusV2.Validator
+dvsRvalidator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| dvsR ||])
+
+{-# INLINABLE dvsR2 #-}
+dvsR2 ::  BuiltinData -> BuiltinData -> BuiltinData -> ()
+dvsR2 datum redeemer _ 
+ | datum == redeemer                = ()
+ | datum == Builtins.mkI 11         = ()
+ | redeemer ==  Builtins.mkI 22    = ()
+ | otherwise                        = error ()
+--to have an error message (on a different script / scriptAddress, you can use traceError function)
+dvsR2validator :: PlutusV2.Validator
+dvsR2validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| dvsR2 ||])
+
+
+---------------------------------------------------------------------------------------------------
+------------------------------------- SERIALIZE THE CONTRACT --------------------------------------------
+
+saveVal :: IO ()
+saveVal = writeValidatorToFile "./assets/gift.plutus" validator
