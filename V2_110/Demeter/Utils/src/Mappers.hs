f518eec48b1533ea111f3af28d099978d61bd580
Roberto <vortecsmaster@vortecsla.com>
2023-05-16 06:50:12 -0500

    including V1 Ledger Api
    including V1 Ledger Api

--- V2_110/Demeter/Utils/src/Mappers.hs
+++ V2_110/Demeter/Utils/src/Mappers.hs
@@ -7,8 +7,8 @@ module Mappers
   ) where
 
 import           PlutusTx
-import           PlutusTx.Builtins           (BuiltinByteString (..))
 import           PlutusTx.Prelude            (Bool, BuiltinData, check, ($),(.))
+import           Plutus.V1.Ledger.Api        (BuiltinByteString (..))
 import           Plutus.V2.Ledger.Api        (CurrencySymbol (CurrencySymbol)
                                              , MintingPolicy
                                              , MintingPolicyHash (MintingPolicyHash)
