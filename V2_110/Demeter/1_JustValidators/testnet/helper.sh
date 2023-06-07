01953f999f9164fdca96a3ae922365eba245141f
Roberto <vortecsmaster@vortecsla.com>
2023-06-07 08:35:35 -0500

    Updated helper shell script
    Updated helper shell script

---
+++ V2_110/Demeter/1_JustValidators/testnet/helper.sh
@@ -0,0 +1,4 @@
+#### Derive address from script
+cardano-cli address build --payment-script-file AlwaysSucceedScript.plutus --testnet-magic 2 --out-file AlwaysSucceedScript.addr
+#### PubKeyHash creation:
+cardano-cli address key-hash --payment-verification-key-file benef1.vkey --out-file benef1.pkh
