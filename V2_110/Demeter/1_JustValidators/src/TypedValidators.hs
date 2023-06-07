f6bdfa12810bf2e8379d4fa4e2a9b7418789d59e
Roberto <vortecsmaster@vortecsla.com>
2023-06-07 00:46:40 -0500

    Updated TypedValidator for makeIsDataIndexed
    include makeIsDataIndexed

--- V2_110/Demeter/1_JustValidators/src/TypedValidators.hs
+++ V2_110/Demeter/1_JustValidators/src/TypedValidators.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DataKinds           #-}  --Enable datatype promotions
 {-# LANGUAGE NoImplicitPrelude   #-}  --Don't load native prelude to avoid conflict with PlutusTx.Prelude
 {-# LANGUAGE TemplateHaskell     #-}  --Enable Template Haskell splice and quotation syntax
+{-# LANGUAGE OverloadedStrings   #-}  --Enable passing strings as other character formats, like bytestring.
 
 module TypedValidators where
 
 --PlutusTx 
-import                  PlutusTx                       (BuiltinData, compile,unstableMakeIsData, makesIsDataIndexed)
+import                  PlutusTx                       (BuiltinData, compile,unstableMakeIsData, makeIsDataIndexed)
 import                  PlutusTx.Prelude               (traceIfFalse, otherwise, (==), Bool (..), Integer, ($))
 import                  Plutus.V2.Ledger.Api        as PlutusV2
 import                  Mappers                        (wrapValidator)
@@ -16,5 +17,4 @@ import                  Prelude                     (IO)
 
 {-# INLINABLE datum22 #-}
 datum22 :: Integer -> () -> () -> ()
-datum22 datum _ _ 
- | datum ==  22         = traceIfFalse
+datum22 datum _ _ = traceIfFalse "Not the right datum!" (datum ==  22)
