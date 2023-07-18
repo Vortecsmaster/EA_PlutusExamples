839a9c518c49bba88ddfea2d57be3a5e230e2cbc
Vortecs <vortecsmaster@vortecsla.com>
2023-07-18 16:36:31 -0500

    Update RockPaperScissors post VM failure
    Losted the ffinal version, taking  learners feedback into consideration for the remake.

--- V2_110/DemeterAndDocker/4_SimpleGames/src/RockPaperScissors.hs
+++ V2_110/DemeterAndDocker/4_SimpleGames/src/RockPaperScissors.hs
@@ -7,7 +7,7 @@ module RockPaperScissors where
 
 --PlutusTx 
 import                  PlutusTx                            (BuiltinData, compile, unstableMakeIsData, makeIsDataIndexed, makeLift)
-import                  PlutusTx.Prelude                    (Eq ,traceIfFalse, otherwise, (==), Bool (..), Integer, ($), (>), (+), (&&), sha2_256)
+import                  PlutusTx.Prelude                    (appendByteString, Eq ,traceIfFalse, otherwise, (==), Bool (..), Integer, ($), (>), (+), (&&), sha2_256)
 import                  Plutus.V1.Ledger.Value          as  PlutusV1
 import                  Plutus.V1.Ledger.Interval           (contains, to) 
 import                  Plutus.V2.Ledger.Api            as  PlutusV2
@@ -22,11 +22,37 @@ import   qualified      Data.ByteString.Char8           as  C
 
 
 --THE ON-CHAIN CODE
+
 data GameOption = Rock | Paper | Scissors --deriving Show for REPL
+type Salt = BuiltinByteString
+data Secret = Secret BuiltinByteString  
 data GameState = Player1 | Player2 | Draw --deriving Show for REPL
+data GameDatum = GD { player1   :: PubKeyHash
+                    , player2   :: PubKeyHash
+                    , p1Secret  :: Secret
+                    , p1option  :: GameOption
+                    , p2option  :: GameOption
+                    , theBet    :: Integer
+                    }
+data Action = GameChoice | P1claim | P2claim | DrawClaim
+
+nOption :: GameOption -> BuiltinByteString
+nOption option = case option of  
+                   Rock     -> "1100"
+                   Paper    -> "3300"
+                   Scissors -> "5500"
+
+
 
 unstableMakeIsData ''GameOption
+unstableMakeIsData ''Secret
 unstableMakeIsData ''GameState
+unstableMakeIsData ''GameDatum
+unstableMakeIsData ''Action
+
+hashSecret :: Salt -> GameOption -> Secret
+hashSecret salt option = Secret $ sha2_256 $ salt `appendByteString` (nOption option) 
+
 
 instance Eq GameOption where
     {-# INLINABLE (==) #-}
@@ -48,54 +74,50 @@ gameState p1 p2
  | p1 == Paper && p2 == Scissors        = Player2
  | p1 == Scissors && p2 == Rock         = Player2
 
+{-# INLINABLE rpsValidator #-}
+rpsValidator :: GameDatum -> Action -> ScriptContext -> Bool
+rpsValidator datum action sContext 
+ | action == GameChoice    = gameChoice
+ | action == P1claim       = p1claim
+ | action == P2claim       = p2claim
+ | action == DrawClaim     = Draw
+ | otherwise               = False
+    where
+        gameChoice :: Bool
+        gameChoice = if (p1Secret datum /= "") then player2choice else somethingElse
+
+        player2choice :: Bool
+        player2choice = (signedBy $ player2 datum) && 
+                        verifyProvidedValue &&
+                        verifyOutputPlayer2 
+
+
+        signedBy :: PubKeyHash -> Bool
+        signedBy pkh = txSignedBy info pkh
  
+        info :: TxInfo
+        info = scriptContextTxInfo sContext
+        
 
+-- mappedValidateGuess :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+-- mappedValidateGuess = wrapValidator validateGuess
 
- 
-newtype HashedString = HashedString BuiltinByteString
-unstableMakeIsData ''HashedString
+-- guessingGameValidator :: Validator
+-- guessingGameValidator =  PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mappedValidateGuess ||])
 
-newtype ClearString = ClearString BuiltinByteString
-unstableMakeIsData ''ClearString
+-- -- {- Serialised Scripts and Values -}
 
-{-# INLINABLE validateGuess #-}
-validateGuess :: HashedString -> ClearString -> ScriptContext -> Bool
-validateGuess hs cs _ = isGoodGuess hs cs
+-- saveValidator  :: IO ()
+-- saveValidator  =  writeValidatorToFile "./testnet/guessingGame.plutus" guessingGameValidator
 
-{-# INLINABLE isGoodGuess #-}
-isGoodGuess :: HashedString -> ClearString -> Bool
-isGoodGuess (HashedString actual) (ClearString guess') = actual == sha2_256 guess'
+-- saveDatum :: IO ()
+-- saveDatum = writeDataToFile "./testnet/secret.json" $ hashString "secretWord"
 
--- create a data script for the guessing game by hashing the string
--- and lifting the hash to its on-chain representation
-hashString :: String -> HashedString
-hashString = HashedString . sha2_256 . toBuiltin . C.pack
+-- saveRedeemer :: IO ()
+-- saveRedeemer = writeDataToFile "./testnet/guess.json" $ clearString "secretWord"  
 
--- create a redeemer script for the guessing game by lifting the
--- string to its on-chain representation
-clearString :: String -> ClearString
-clearString = ClearString . toBuiltin . C.pack
-
-mappedValidateGuess :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-mappedValidateGuess = wrapValidator validateGuess
-
-guessingGameValidator :: Validator
-guessingGameValidator =  PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mappedValidateGuess ||])
-
--- {- Serialised Scripts and Values -}
-
-saveValidator  :: IO ()
-saveValidator  =  writeValidatorToFile "./testnet/guessingGame.plutus" guessingGameValidator
-
-saveDatum :: IO ()
-saveDatum = writeDataToFile "./testnet/secret.json" $ hashString "secretWord"
-
-saveRedeemer :: IO ()
-saveRedeemer = writeDataToFile "./testnet/guess.json" $ clearString "secretWord"  
-
-saveAll :: IO ()
-saveAll = do
-            saveValidator
-            saveDatum
-            saveRedeemer
-
+-- saveAll :: IO ()
+-- saveAll = do
+--             saveValidator
+--             saveDatum
+--             saveRedeemer
