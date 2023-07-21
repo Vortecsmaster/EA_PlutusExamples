ae8cb0ce453352085d622435eb777216646fa7c2
Vortecs <vortecsmaster@vortecsla.com>
2023-07-21 12:02:51 -0500

    implantes.... intereferencias...
    y la opcion contraria para cuando??

--- V2_110/DemeterAndDocker/4_SimpleGames/src/RockPaperScissors.hs
+++ V2_110/DemeterAndDocker/4_SimpleGames/src/RockPaperScissors.hs
@@ -22,6 +22,7 @@ import                  Prelude                             hiding (Eq,(==),(&&)
 --Extra
 import                  Text.Printf                         (printf)
 import   qualified      Data.ByteString.Char8           as  C
+import Data.Time.Clock.POSIX (POSIXTime)
 
 
 --THE ON-CHAIN CODE
@@ -31,12 +32,15 @@ type Salt = BuiltinByteString
 data Secret = Secret BuiltinByteString  
 data GameState = Player1 | Player2 | Reveal --deriving Show for REPL
 data GameOutcome = Player1wins | Player2wins | Draw
-data GameDatum = GD { player1   :: PubKeyHash
-                    , player2   :: PubKeyHash
-                    , p1Secret  :: Secret
-                    , p1option  :: GameOption
-                    , p2option  :: GameOption
-                    , theBet    :: Integer
+data GameDatum = GD { player1    :: PubKeyHash
+                    , player2    :: PubKeyHash
+                    , p1Secret   :: Secret
+                    , p1option   :: GameOption
+                    , p2option   :: GameOption
+                    , gametime   :: POSIXTime
+                    , revealtime :: POSIXTime
+                    , theBet     :: Integer
+                    , gameState  :: GameState
                     }
 data Action = GameChoice Player | P1claim | P2claim | DrawClaim deriving (Eq)
 
@@ -80,28 +84,57 @@ gameState p1 p2
 {-# INLINABLE rpsValidator #-}
 rpsValidator :: GameDatum -> Action -> ScriptContext -> Bool
 rpsValidator datum action sContext 
- | action == (GameChoice 1)  = gameChoice 1 
  | action == (GameChoice 2)  = gameChoice 2 
+ | action == (GameChoice 1)  = gameChoice 1 
  | action == P1claim       = True --p1claim
  | action == P2claim       = True --p2claim
  | action == DrawClaim     = True --Draw
  | otherwise               = False
     where
         gameChoice :: Integer -> Bool
-        gameChoice 2 = True --if (p1Secret datum /= "") then player2choice else somethingElse
-        gameChoice 1 = True
+        gameChoice 2 = True 
+        gameChoice 1 = True    
+
+        -- findUTxOdatum :: OutputDatum
+        -- findUTxOdatum = case findOwnInput sContext of
+        --     Just utxo    ->  txOutDatum $ txInInfoResolved utxo
+        --     Nothing      ->  NoOutputDatum 
+            
+        -- Current UTxO been consumed DATUM must have the gameState == Player1
+        --if (p1Secret datum /= "") then player2choice else somethingElse
+        
+
+
+
+
 
         player2choice :: Bool
-        player2choice = True --(signedBy $ player2 datum) && 
+        player2choice = 
+            
+            traceIfFalse "Must be signed by Player!" 
+            
+            True --(signedBy $ player2 datum) && 
                         --verifyProvidedValue &&
                         --verifyOutputPlayer2 
+
+        checkGameState :: GameState -> Bool    
+        checkGameState state = True
 
         signedBy :: PubKeyHash -> Bool
         signedBy pkh = txSignedBy info pkh
  
         info :: TxInfo
         info = scriptContextTxInfo sContext
-        
+{-
+{-# INLINABLE parseOracleDatum #-}
+parseOracleDatum :: TxOut -> TxInfo -> Maybe Integer
+parseOracleDatum o info = case txOutDatum o of
+    NoOutputDatum -> Nothing
+    OutputDatum (Datum d) -> PlutusTx.fromBuiltinData d
+    OutputDatumHash dh -> do
+                        Datum d <- findDatum dh info
+                        PlutusTx.fromBuiltinData d    
+-}       
 
 -- mappedValidateGuess :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 -- mappedValidateGuess = wrapValidator validateGuess
